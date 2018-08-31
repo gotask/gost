11d0aaad2ed62195e5ffeb486e842cb1db88693f
yezi <shujianhenu@foxmail.com>
2018-08-31 19:48:06 +0800

    Service Simple Imp

--- stnet/simpleimp.go
+++ stnet/simpleimp.go
@@ -6,71 +6,57 @@ import (
 	"net/http"
 )
 
-//ServiceImpEcho
-type ServiceEcho struct {
+//ServiceImpBase
+type ServiceBase struct {
 }
 
-func (service *ServiceEcho) Init() bool {
+func (service *ServiceBase) Init() bool {
 	return true
 }
-func (service *ServiceEcho) Loop() {
+func (service *ServiceBase) Loop() {
 
 }
-func (service *ServiceEcho) Destroy() {
+func (service *ServiceBase) Destroy() {
 
 }
-func (service *ServiceEcho) HandleMessage(sess *Session, msgID uint32, msg interface{}) {
+func (service *ServiceBase) HandleMessage(sess *Session, msgID uint32, msg interface{}) {
 
 }
-func (service *ServiceEcho) Unmarshal(sess *Session, data []byte) (lenParsed int, msgID int32, msg interface{}, err error) {
-	sess.Send(data)
-	return len(data), -1, nil, nil
-}
-func (service *ServiceEcho) SessionOpen(sess *Session) {
+func (service *ServiceBase) SessionOpen(sess *Session) {
 
 }
-func (service *ServiceEcho) SessionClose(sess *Session) {
+func (service *ServiceBase) SessionClose(sess *Session) {
 
 }
-func (service *ServiceEcho) HeartBeatTimeOut(sess *Session) {
+func (service *ServiceBase) HeartBeatTimeOut(sess *Session) {
 
 }
-func (service *ServiceEcho) HandleError(sess *Session, err error) {
+func (service *ServiceBase) HandleError(sess *Session, err error) {
 	SysLog.Error(err.Error())
+}
+
+//ServiceImpEcho
+type ServiceEcho struct {
+	ServiceBase
+}
+
+func (service *ServiceEcho) Unmarshal(sess *Session, data []byte) (lenParsed, processorID int, msgID int32, msg interface{}, err error) {
+	sess.Send(data)
+	return len(data), int(sess.GetID() % uint64(ProcessorThreadsNum)), -1, nil, nil
 }
 
 //ServiceHttp
 type ServiceHttp struct {
+	ServiceBase
 }
 
-func (service *ServiceHttp) Init() bool {
-	return true
-}
-func (service *ServiceHttp) Loop() {
-
-}
-func (service *ServiceHttp) Destroy() {
-
-}
 func (service *ServiceHttp) HandleMessage(sess *Session, msgID uint32, msg interface{}) {
 	//req:=msg.(*http.Request)
 }
-func (service *ServiceHttp) Unmarshal(sess *Session, data []byte) (lenParsed int, msgID int32, msg interface{}, err error) {
+func (service *ServiceHttp) Unmarshal(sess *Session, data []byte) (lenParsed, processorID int, msgID int32, msg interface{}, err error) {
 	req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(data)))
 	if err != nil {
-		return 0, 0, nil, nil
+		return 0, 0, 0, nil, nil
 	}
-	return len(data), 0, req, nil
-}
-func (service *ServiceHttp) SessionOpen(sess *Session) {
-
-}
-func (service *ServiceHttp) SessionClose(sess *Session) {
-
-}
-func (service *ServiceHttp) HeartBeatTimeOut(sess *Session) {
-
-}
-func (service *ServiceHttp) HandleError(sess *Session, err error) {
-	SysLog.Error(err.Error())
+	return len(data), int(sess.GetID() % uint64(ProcessorThreadsNum)), 0, req, nil
 }
