452de66edcc7e5f38bd0edf25e5ba345c7b6e3d7
yezi <shujianhenu@foxmail.com>
2019-07-25 23:26:11 +0800

    read write int to []byte

--- stutil/unsafe.go
+++ stutil/unsafe.go
@@ -1,14 +1,15 @@
 package stutil
 
 import (
+	"io"
 	"unsafe"
 )
 
-func RUint16(b []byte) uint16 {
+func RUint16(b []byte) (x uint16) {
 	_ = b[1]
-	var v uint16
-	v = *(*uint16)(unsafe.Pointer(&b[0]))
-	return v
+	x = uint16(b[0])
+	x |= uint16(b[1]) << 8
+	return
 }
 
 func WUint16(b []byte, x uint16) {
@@ -18,21 +19,21 @@ func WUint16(b []byte, x uint16) {
 }
 
 func RInt16(b []byte) int16 {
-	_ = b[1]
-	var v int16
-	v = *(*int16)(unsafe.Pointer(&b[0]))
-	return v
+	v := RUint16(b)
+	return int16(v)
 }
 
 func WInt16(b []byte, x int16) {
 	WUint16(b, uint16(x))
 }
 
-func RUint32(b []byte) uint32 {
+func RUint32(b []byte) (x uint32) {
 	_ = b[3]
-	var v uint32
-	v = *(*uint32)(unsafe.Pointer(&b[0]))
-	return v
+	x = uint32(b[0])
+	x |= uint32(b[1]) << 8
+	x |= uint32(b[2]) << 16
+	x |= uint32(b[3]) << 24
+	return
 }
 
 func WUint32(b []byte, x uint32) {
@@ -44,10 +45,8 @@ func WUint32(b []byte, x uint32) {
 }
 
 func RInt32(b []byte) int32 {
-	_ = b[3]
-	var v int32
-	v = *(*int32)(unsafe.Pointer(&b[0]))
-	return v
+	v := RUint32(b)
+	return int32(v)
 }
 
 func WInt32(b []byte, x int32) {
@@ -64,11 +63,17 @@ func WFloat32(b []byte, x float32) {
 	WUint32(b, v)
 }
 
-func RUint64(b []byte) uint64 {
+func RUint64(b []byte) (x uint64) {
 	_ = b[7]
-	var v uint64
-	v = *(*uint64)(unsafe.Pointer(&b[0]))
-	return v
+	x = uint64(b[0])
+	x |= uint64(b[1]) << 8
+	x |= uint64(b[2]) << 16
+	x |= uint64(b[3]) << 24
+	x |= uint64(b[4]) << 32
+	x |= uint64(b[5]) << 40
+	x |= uint64(b[6]) << 48
+	x |= uint64(b[7]) << 56
+	return
 }
 
 func WUint64(b []byte, x uint64) {
@@ -84,10 +89,8 @@ func WUint64(b []byte, x uint64) {
 }
 
 func RInt64(b []byte) int64 {
-	_ = b[3]
-	var v int64
-	v = *(*int64)(unsafe.Pointer(&b[0]))
-	return v
+	v := RUint64(b)
+	return int64(v)
 }
 
 func WInt64(b []byte, x int64) {
@@ -102,4 +105,39 @@ func RFloat64(b []byte) float64 {
 func WFloat64(b []byte, x float64) {
 	v := *(*uint64)(unsafe.Pointer(&x))
 	WUint64(b, v)
+}
+
+func RVarint(b []byte, x uint64) (int, error) {
+	var n int
+	for n = 0; x > 127; n++ {
+		if n >= len(b) {
+			return 0, io.ErrShortBuffer
+		}
+		b[n] = 0x80 | uint8(x&0x7F)
+		x >>= 7
+	}
+	if n >= len(b) {
+		return 0, io.ErrShortBuffer
+	}
+	b[n] = uint8(x)
+	n++
+	return n, nil
+}
+
+func WVarint(b []byte) (x uint64, n int, e error) {
+	// x, n already 0
+	for shift := uint(0); shift < 64; shift += 7 {
+		if n >= len(b) {
+			return 0, 0, io.ErrUnexpectedEOF
+		}
+		b := uint64(b[n])
+		n++
+		x |= (b & 0x7F) << shift
+		if (b & 0x80) == 0 {
+			return x, n, nil
+		}
+	}
+
+	// The number is too large to represent in a 64-bit value.
+	return 0, 0, io.ErrUnexpectedEOF
 }
